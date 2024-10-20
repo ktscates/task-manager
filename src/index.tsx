b930ca5c6dbf8fbd1924d312be05fffe32a0edb3
ktscates <cates2506@gmail.com>
2024-10-20 17:00:10 +0200

    translation setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { I18nextProvider, i1 } from "react-i18next";
+import i18n from "./i18n";
 
 const queryClient = new QueryClient();
 
@@ -11,14 +13,16 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tasks" element={TaskPage} />
-      </Routes>
-    </Router>
-  </QueryClientProvider>
+  <I18nextProvider i18n={i18n}>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tasks" element={TaskPage} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
