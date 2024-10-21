a999bd42729cfa0651042808fb4dc39f1edeed7c
ktscates <cates2506@gmail.com>
2024-10-21 13:17:58 +0200

    project setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { I18nextProvider, i1 } from "react-i18next";
-import i18n from "./i18n";
-
-const queryClient = new QueryClient();
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/tasks" element={TaskPage} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
-  </I18nextProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
